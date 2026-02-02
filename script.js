0f900c1479e74f175f69c742509b731dbdafef5a
Boby Keshri <bobyk25122000>
2026-02-02 09:31:28 +0530

    againUpdate

--- script.js
+++ script.js
@@ -28,6 +28,28 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-list a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
 
 //! Title
 document.addEventListener("DOMContentLoaded", function() {
